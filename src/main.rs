9394bba28ed99eb471b57e8fe28390ada6c2d3f7
wander <wander@ffactory.org>
2023-05-16 18:07:46 +0800

    宏布局

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 use gtk::glib::clone;
 use gtk::prelude::*;
-use relm4::prelude::*;
+use gtk::Window;
+use relm4::{prelude::*, view};
 use relm4::{ComponentSender, SimpleComponent};
-
-fn main() {
-    let app = RelmApp::new("relm4.test.simple_manual");
-    app.run::<AppModel>(0);
-}
 
 #[derive(Debug)]
 enum AppInput {
@@ -22,6 +18,7 @@ struct AppWidgets {
     label: gtk::Label,
 }
 
+#[relm4::component]
 impl SimpleComponent for AppModel {
     type Input = AppInput;
 
@@ -29,54 +26,101 @@ impl SimpleComponent for AppModel {
 
     type Init = u8;
 
-    type Root = gtk::Window;
+    view! {
+        gtk::Window {
+            set_title:Some("Simple app"),
+            set_default_width:300,
+            set_default_height:100,
+            gtk::Box {
+                set_orientation:gtk::Orientation::Vertical,
+                set_spacing:5,
+                set_margin_all:5,
 
-    type Widgets = AppWidgets;
-    fn init_root() -> Self::Root {
-        gtk::Window::builder()
-            .title("Simple app")
-            .default_width(300)
-            .default_height(100)
-            .build()
+                gtk::Button {
+                    set_label: "加1",
+                    connect_clicked[sender]=> move |_|{
+                        sender.input(AppInput::Increment);
+
+                    }
+                },
+
+                gtk::Button::with_label("减1") {
+                    connect_clicked[sender] => move |_| {
+                        sender.input(AppInput::Decrement);
+                    }
+
+                },
+                gtk::Label {
+                    #[watch]
+                    set_label: &format!("累计： {}",model.counter),
+                    set_margin_all:5,
+                },
+
+            }
+        }
     }
 
-    /// Initialize the UI and model.
+    // Initialize the UI.
     fn init(
         counter: Self::Init,
-        window: &Self::Root,
+        root: &Self::Root,
         sender: ComponentSender<Self>,
-    ) -> relm4::ComponentParts<Self> {
+    ) -> ComponentParts<Self> {
         let model = AppModel { counter };
 
-        let vbox = gtk::Box::builder()
-            .orientation(gtk::Orientation::Vertical)
-            .spacing(5)
-            .build();
-
-        let inc_button = gtk::Button::with_label("加1");
-        let dec_button = gtk::Button::with_label("减1");
-
-        let label = gtk::Label::new(Some(&format!("Counter: {}", model.counter)));
-        label.set_margin_all(5);
-
-        window.set_child(Some(&vbox));
-        vbox.set_margin_all(5);
-        vbox.append(&inc_button);
-        vbox.append(&dec_button);
-        vbox.append(&label);
-
-        inc_button.connect_clicked(clone!(@strong sender => move |_| {
-            sender.input(AppInput::Increment);
-        }));
-
-        dec_button.connect_clicked(clone!(@strong sender => move |_| {
-            sender.input(AppInput::Decrement);
-        }));
-
-        let widgets = AppWidgets { label };
+        // Insert the macro code generation here
+        let widgets = view_output!();
 
         ComponentParts { model, widgets }
     }
+    // type Root = gtk::Window;
+
+    // type Widgets = AppWidgets;
+    // fn init_root() -> Self::Root {
+    //     gtk::Window::builder()
+    //         .title("Simple app")
+    //         .default_width(300)
+    //         .default_height(100)
+    //         .build()
+    // }
+
+    // /// Initialize the UI and model.
+    // fn init(
+    //     counter: Self::Init,
+    //     window: &Self::Root,
+    //     sender: ComponentSender<Self>,
+    // ) -> relm4::ComponentParts<Self> {
+    //     let model = AppModel { counter };
+
+    //     let vbox = gtk::Box::builder()
+    //         .orientation(gtk::Orientation::Vertical)
+    //         .spacing(5)
+    //         .build();
+
+    //     let inc_button = gtk::Button::with_label("加1");
+    //     let dec_button = gtk::Button::with_label("减1");
+
+    //     let label = gtk::Label::new(Some(&format!("累计: {}", model.counter)));
+    //     label.set_margin_all(5);
+
+    //     window.set_child(Some(&vbox));
+    //     vbox.set_margin_all(5);
+    //     vbox.append(&inc_button);
+    //     vbox.append(&dec_button);
+    //     vbox.append(&label);
+
+    //     inc_button.connect_clicked(clone!(@strong sender => move |_| {
+    //         sender.input(AppInput::Increment);
+    //     }));
+
+    //     dec_button.connect_clicked(clone!(@strong sender => move |_| {
+    //         sender.input(AppInput::Decrement);
+    //     }));
+
+    //     let widgets = AppWidgets { label };
+
+    //     ComponentParts { model, widgets }
+    // }
 
     fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {
         match message {
@@ -88,8 +132,13 @@ impl SimpleComponent for AppModel {
             }
         }
     }
-    /// Update the view to represent the updated model.
-    fn update_view(&self, widgets: &mut Self::Widgets, _sender: ComponentSender<Self>) {
-        widgets.label.set_label(&format!("合计: {}", self.counter));
-    }
+    // Update the view to represent the updated model.
+    // fn update_view(&self, widgets: &mut Self::Widgets, _sender: ComponentSender<Self>) {
+    //     widgets.label.set_label(&format!("累计: {}", self.counter));
+    // }
+}
+
+fn main() {
+    let app = RelmApp::new("relm4.test.simple_manual");
+    app.run::<AppModel>(0);
 }
